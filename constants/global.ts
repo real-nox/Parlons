f99a8e61eff72c53ca84dd5c31b4452ed46697f7
Rayane <sirri.rayane.ma@gmail.com>
2026-09-20 15:27:39 +0000

    added subtitles

--- constants/global.ts
+++ constants/global.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 export const globalColors = {
   title2: "rgb(0, 77, 165)",
   link: "#3d40ff",
+  subtitle: "#a7a7a7",
 };
 
 export const globalStyle = StyleSheet.create({
@@ -10,7 +11,11 @@ export const globalStyle = StyleSheet.create({
     paddingBottom: 10,
     fontWeight: "bold",
   },
-  subtitle: {
+  h3: {
     fontSize: 16,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: globalColors.subtitle,
   },
 });
